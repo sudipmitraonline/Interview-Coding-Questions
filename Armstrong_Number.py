cc1305f4310456a21d68871a0528793d13f7fd99
Sudip Mitra <44464604+sudipmitraonline@users.noreply.github.com>
2021-12-03 20:21:37 +0530

    Create Armstrong_Number.py

---
+++ Armstrong_Number.py
@@ -0,0 +1,26 @@
+ 
+# taking input and storing in num variable
+num = int(input("Enter a number: "))
+ 
+# finding order using len() and str() function
+order = len(str(num))   
+             
+# initializing the sum variable by 0
+sum = 0                      
+# storing the num in temp variable                       
+temp = num                  
+   
+# going through every digit of number store in temp variable                     
+while temp > 0:                                     
+    n = temp % 10
+    temp = temp // 10
+    # storing the sum of cube of the digits in sum variable
+    sum = sum + pow(n,order)   
+           
+# checking whether the sum is equal to num or not               
+if(sum == num):                 
+    # printing the num is an Armstrong number                   
+    print(num,"is an Armstrong Number.")          
+else:
+    # printing the num is not an Armstrong number
+    print(num,"is not an Armstrong Number.")
